24ceeb87c2ebb9beac8f2c0081deb9959bb40249
Jason Zhen <jzhenxiang@users.noreply.github.com>
2026-04-13 23:18:09 +0800

    docs(offset): clarify concurrency guarantee and file-creation behaviour

--- internal/offset/doc.go
+++ internal/offset/doc.go
@@ -5,6 +5,9 @@
 // the correct position after a process restart without reprocessing already
 // delivered events.
 //
+// The tracker is safe for concurrent use. Commits are written atomically to
+// disk using a write-and-rename strategy to avoid partial writes on crash.
+//
 // Usage:
 //
 //	tracker, err := offset.New("/var/lib/pgstream/offset.json")
@@ -15,4 +18,7 @@
 //
 //	// on startup, retrieve the resume point
 //	fmt.Println(tracker.Current())
+//
+// If the offset file does not exist when New is called, the tracker starts
+// from zero and will create the file on the first successful Commit.
 package offset
